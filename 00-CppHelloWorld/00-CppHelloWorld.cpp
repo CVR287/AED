4981c150dd4c27a9834e30588ef66e49f4f048eb
Curt Venator R <103224431+CVR287@users.noreply.github.com>
2022-04-19 22:42:22 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/00-CppHelloWorld.cpp
@@ -0,0 +1,27 @@
+//
+// 00-CppHelloWorld.cpp
+//
+// Profesor     : José María Sola
+// Alumno       : Curt VENATOR RAUCH
+// L.U.         : 083480-6
+// Curso        : K1253 (N)
+// Año          : 2022
+//
+// IDE          : Microsoft Visual Studio Community 2022
+// Versión      : 17.1.4
+// Release      : 17.1.4+32407.343
+// Framework    : Microsoft .NET Framework v4.8.04084
+// C++ Estándar : ISO C++20 (/std:c++20)
+// Compilador   : MSBuild.exe v17.1.0+ae57d105c
+//
+// T.P. Nº      : 00
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "\n\tHello world!." << std::endl;
+
+	return 0;
+}

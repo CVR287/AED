c2f1938b67256d1e5a31f954d41a5c29a6ea1237
Curt Venator R <103224431+CVR287@users.noreply.github.com>
2022-04-19 23:15:03 -0300

    Update 00-CppHelloWorld.cpp

--- 00-CppHelloWorld/00-CppHelloWorld.cpp
+++ 00-CppHelloWorld/00-CppHelloWorld.cpp
@@ -1,20 +1,22 @@
-//
 // 00-CppHelloWorld.cpp
+// Escribir y compilar el programa b√°sico y cl√°sico "Hello world".
+// CuVR
+// 20220419
 //
-// Profesor     : JosÈ MarÌa Sola
+// Profesor     : Jos√© Mar√≠a Sola
 // Alumno       : Curt VENATOR RAUCH
 // L.U.         : 083480-6
 // Curso        : K1253 (N)
-// AÒo          : 2022
+// A√±o          : 2022
 //
 // IDE          : Microsoft Visual Studio Community 2022
-// VersiÛn      : 17.1.4
+// Versi√≥n      : 17.1.4
 // Release      : 17.1.4+32407.343
 // Framework    : Microsoft .NET Framework v4.8.04084
-// C++ Est·ndar : ISO C++20 (/std:c++20)
+// C++ Est√°ndar : ISO C++20 (/std:c++20)
 // Compilador   : MSBuild.exe v17.1.0+ae57d105c
 //
-// T.P. N∫      : 00
+// T.P. N¬∫      : 00
 //
 
 #include <iostream>
